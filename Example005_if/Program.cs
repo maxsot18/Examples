d18395c99543a57917e19ec864ca3578adc2f81c
Maxsot <sotnikov.maxim3007@gmail.com>
2022-11-22 08:28:30 -0800

    Example005_if

---
+++ Example005_if/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Write your name: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "max")
+{
+    Console.WriteLine("Yeah, This is Max. The Best Of The Best");
+}
+else
+{
+    Console.Write("Hey, ");
+    Console.Write(username);
+}
+
