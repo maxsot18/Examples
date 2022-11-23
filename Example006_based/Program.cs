beeb2c8b1961b7a8f8f3ab57e716a5348fa705bd
Maxsot <sotnikov.maxim3007@gmail.com>
2022-11-23 05:47:26 -0800

    Example006_based

---
+++ Example006_based/Program.cs
@@ -0,0 +1,12 @@
+﻿int a = 15;
+int b = 109;
+int c = 9;
+int d = 1;
+
+int max = a;
+
+if (max < b) max = b;
+if (max < c) max = c;
+if (max < d) max = d;
+Console.Write("max = ");
+Console.Write(max);
