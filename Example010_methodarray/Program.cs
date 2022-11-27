420366525bde657810b5a7c71f982d137716cfc0
Maxsot <sotnikov.maxim3007@gmail.com>
2022-11-27 06:04:37 -0800

    Example010_methodarray

---
+++ Example010_methodarray/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] array = {89, 66, 3, 4, 150, 14, -12, 7, 150};
+
+int n = array.Length;
+int find = 150; 
+int index = 0;
+
+while(index < n)
+{
+    if(array[index] == find) 
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
